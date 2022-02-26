3ecbd7bd1caf1f34bf3fa116e086b1504818ad59
eloo95 <elvis-halilovic@outlook.com>
2022-02-26 03:53:55 +0100

    Initial commit

---
+++ src/main/java/dev/eloo/lib/ElooLibrary.java
@@ -0,0 +1,17 @@
+package dev.eloo.lib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElooLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
